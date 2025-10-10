964eb93257245f645822c7f62c1c3a10f45af065
marina-popova11 <181482786+marina-popova11@users.noreply.github.com>
2025-10-10 21:14:03 +0300

    Added the CommandList and CommandGet for Client

--- Homework4/SimpleFTP/FTPClient/Client.cs
+++ Homework4/SimpleFTP/FTPClient/Client.cs
@@ -4,16 +4,21 @@
 
 namespace FTPClient;
 
+using System.Net;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 
 /// <summary>
 /// Class for ftp client,
 /// you can listing files and
 /// get a file from server.
 /// </summary>
-public class Client : IDisposable
+public class Client
 {
     private TcpClient client;
+    private StreamWriter writer;
+    private StreamReader reader;
+    private bool isConnected;
     private bool isDispose;
 
     /// <summary>
@@ -22,13 +27,126 @@ public class Client : IDisposable
     public Client()
     {
         this.client = new TcpClient();
+        this.isConnected = false;
+    }
+
+    /// <summary>
+    /// Gets flag for isConnected.
+    /// </summary>
+    /// <returns>True or false.</returns>
+    public bool IsConnect() => this.isConnected;
+
+    /// <summary>
+    /// Connects the client to the server.
+    /// </summary>
+    /// <param name="ip">Ip address for connect to.</param>
+    /// <param name="port">Port number for connect to.</param>
+    /// <returns>Completed task.</returns>
+    public async Task Connect(IPAddress ip, int port)
+    {
+        await this.client.ConnectAsync(ip, port);
+        var netStream = this.client.GetStream();
+        using (this.writer = new StreamWriter(netStream))
+        using (this.reader = new StreamReader(netStream))
+        {
+            this.isConnected = true;
+        }
+    }
+
+    /// <summary>
+    /// Viewing files in a directory on the server.
+    /// </summary>
+    /// <param name="path">What the file will be viewed.</param>
+    /// <returns>List of files in directory.</returns>
+    public async Task<List<FileInfo>> CommandList(string path)
+    {
+        if (!this.IsConnect())
+        {
+            await this.writer.WriteLineAsync("Client does not connect!");
+            return new List<FileInfo>();
+        }
+
+        await this.writer.WriteLineAsync($"1 {path}");
+        var response = await this.reader.ReadLineAsync();
+        if (response == "-1")
+        {
+            return new List<FileInfo>();
+        }
+
+        if (!int.TryParse(response, out var size) || size < 0)
+        {
+            return new List<FileInfo>();
+        }
+
+        var list = new List<FileInfo>();
+        for (int i = 0; i < size; ++i)
+        {
+            var line = await this.reader.ReadLineAsync();
+            var parts = line!.Split(' ');
+            var fileObject = new FileInfo(parts[1]);
+            list.Add(fileObject);
+
+            // list.Add(new FileInfo
+            // {
+            //     Size = long.Parse(parts[0]),
+            //     Name = parts[1],
+            //     IsDir = bool.Parse(parts[3]),
+            // });
+        }
+
+        return list;
+    }
+
+    /// <summary>
+    /// Downloading a file from server.
+    /// </summary>
+    /// <param name="path">What the file will be copy.</param>
+    /// <param name="localPath">Where file will be download to.</param>
+    /// <returns>True if file was successfully downloaded, false overwise.</returns>
+    public async Task<bool> CommandGet(string path, string localPath)
+    {
+        if (!this.IsConnect())
+        {
+            await this.writer.WriteLineAsync("Client does not connect!");
+            return false;
+        }
+
+        await this.writer.WriteLineAsync($"2 {path}");
+        var response = await this.reader.ReadLineAsync();
+        if (response == "-1")
+        {
+            return false;
+        }
+
+        if (!long.TryParse(response, out var size) || size < 0)
+        {
+            return false;
+        }
+
+        using (var localFile = File.Create(localPath))
+        using (var netStream = this.client.GetStream())
+        {
+            var buffer = new byte[4096];
+            var readBytes = 0;
+            while (readBytes < size)
+            {
+                var remainingBytes = await netStream.ReadAsync(buffer, 0, (int)Math.Min(buffer.Length, size - readBytes));
+                await localFile.WriteAsync(buffer, 0, remainingBytes);
+                ++readBytes;
+            }
+        }
+
+        return true;
     }
 
     /// <summary>
     /// Terminates the client's work.
     /// </summary>
-    public void Dispose()
+    /// <returns>Completed task.</returns>
+    public async Task Disconnect()
     {
-        this.client.Dispose();
+        this.reader.Close();
+        this.writer.Close();
+        this.client.Close();
     }
 }
